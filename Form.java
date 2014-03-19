27c1618b8a1f6f66d031fa3b04e703554206534b
tinafanxr <fanxiurong2002@163.com>
2014-03-19 09:36:01 +0800

    write in home.

--- Form.java
+++ Form.java
@@ -1,19 +1,22 @@
-package com.fanxr.test;
+package com.fanxr;
 
 import java.io.File;
 import java.util.List;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class Form {
 
 	public static void main(String[] args) throws InterruptedException {
 	
-		 WebDriver dr = new FirefoxDriver();
+		 WebDriver dr = new ChromeDriver();
          
          //指定所有打开的文件
         File file = new File("src/form.html" );
@@ -53,12 +56,20 @@ public class Form {
         }
         Thread.sleep(1000);
         
+       
+        
         //选择按钮并点击
         WebElement bt = dr.findElement(By.className("btn"));
         System.out.println(bt.getText());
         System.out.println("value 的值是： " + bt.getAttribute("value"));
         System.out.println("type的值是： " + bt.getAttribute("type"));
         bt.click();
+        Thread.sleep(1000);
+        
+        //点击弹出alert的确定按钮。
+        Alert alert = dr.switchTo().alert();
+        System.out.println(alert.getText());
+        alert.accept();
         Thread.sleep(1000);
         
         
